b753f508c539ce0dfaf6be39e832c3e4136d1248
Ednardo-Matos <ednardoinacio@outlook.com>
2021-04-12 19:24:48 -0300

    atualizaçao

--- src/Playlist.js
+++ src/Playlist.js
@@ -6,14 +6,18 @@ import reggae from './imagens/reggae.jpeg';
 import funk from './imagens/funk.jpg';
 import pop from './imagens/pop.jpg';
 import mpb from './imagens/mpb.jpg';
+
 /*rock */
 import nandoresis from './musicas/nandoresis.mp3';
+import wandinho from './musicas/exagerado.mp3'
+
 /*forro*/
 import avioes from './musicas/avioes.mp3';
 import tarcisio from './musicas/tarcisio.mp3';
+
 /*reggae*/
-import wandinho from './musicas/sorri.mp3';
-import wandinho2 from './musicas/exagerado.mp3';
+import sorri from './musicas/sorri.mp3';
+import revolution from './musicas/revolution.mp3';
 
 
 
@@ -27,14 +31,14 @@ import wandinho2 from './musicas/exagerado.mp3';
 export default function Playlist(){
 
         const playlists = [
-            {id: 1, nome: 'Rock', capa: 'rock.jpeg', musicas:[{id: 1, nome: "Nando Reis - Livre", aquivo: "nandoresis.mp3"},{}]},
+            {id: 1, nome: 'Rock', capa: 'rock.jpeg', musicas:[{id: 1, nome: "Nando Reis - Livre", aquivo: "nandoresis.mp3"}, {id: 2, nome: "Wandinho - exagerado", aquivo: "exagerado.mp3"}]},
 
             {id: 2, nome: 'Forro', capa: 'forro.jpg', musicas:[{id: 1, nome: "Ela aparte minha mente", aquivo: "avioes.mp3"},{id: 2, nome: "meia noite você tem meu.mp3", aquivo: "tarcisio.mp3"}]},
             
             {id: 3, nome: 'Sertanejo', capa: 'sertanejo.jpg', musicas:[{id: 1, nome: "musica teste", aquivo: "musica.mp3"},{}]},
             {id: 4, nome: 'Samba', capa: 'samba.jpg', musicas:[{id: 1, nome: "musica teste", aquivo: "musica.mp3"},{}]},
 
-            {id: 5, nome: 'Reggae', capa: 'reggae.jpeg', musicas:[{id: 1, nome: "Wandinho - exagerado", aquivo: "exagerado.mp3"},{}]},
+            {id: 5, nome: 'Reggae', capa: 'reggae.jpeg', musicas:[{id: 1, nome: "Sorri", aquivo: "sorri.mp3"},{id: 2, nome: "Revolution", aquivo: "revolution.mp3"}]},
 
             {id: 6, nome: 'Funk', capa: 'funk.jpg', musicas:[{id: 1, nome: "musica teste", aquivo: "musica.mp3"},{}]},
             {id: 7, nome: 'Pop', capa: 'pop.jpg', musicas:[{id: 1, nome: "musica teste", aquivo: "musica.mp3"},{}]},
